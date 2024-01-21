839b8d36dfbffffddfbd1caacc6781811eddedde
neukkken <santyagonarvaezns@gmail.com>
2024-01-21 14:52:17 -0500

    v1.0.0.1

--- models/LolEsports.model.teams.js
+++ models/LolEsports.model.teams.js
@@ -3,7 +3,8 @@ import mongoose, { Schema } from "mongoose";
 const teams=new Schema({
     name: String,
     league: String,
-    logo: String
+    logo: String,
+    description: String
 })
 
 export default mongoose.model('Teams', teams)
--- routes/LolEsports.routes.js
+++ routes/LolEsports.routes.js
@@ -1,7 +1,9 @@
 import { getAllTeams, postTeams } from "../controllers/LolEsports.controller.teams.js";
 import { getAllPlayers, postPlayers } from "../controllers/LolEsports.controller.players.js";
 import { getAllNews, postNews } from "../controllers/LolEsports.controller.news.js";
+
 import { Router } from "express";
+
 
 const router = Router()
 
@@ -12,7 +14,6 @@ router.get('/players', getAllPlayers)
 router.post('/players', postPlayers)
 router.get('/news', getAllNews)
 router.post('/news', postNews)
-
 
 
 export default router
